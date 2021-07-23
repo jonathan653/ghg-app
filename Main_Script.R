4869d1f71a6ad0e0760e2b2fee65669f67257447
jonbr407 <brett.jonesnz@gmail.com>
2021-07-23 15:54:08 +1200

    Add main script

---
+++ Main_Script.R
@@ -0,0 +1,31 @@
+#All the package installs for RSQLite database, reading xls files
+#and tools needed.
+install.packages("RSQLite")
+library(RSQLite)
+install.packages('tidyverse')
+library(tidyverse)
+install.packages('Rcpp')
+library(Rcpp)
+
+library(DBI)
+library(readxl)
+
+#Read spreadsheet into R
+Climate_Data_Table <- read_excel("Net Carbon Zero and impact of different variables July 2021.xlsx")
+
+#Connecting database created in SQLite.
+mydb <- dbConnect(RSQLite::SQLite(), "Climate_Data.sqlite")
+
+#Writing Climate_Data_Table into Climate_Data database in SQLite.
+dbWriteTable(mydb, "Climate_Data_Table", Climate_Data_Table)
+dbListTables(mydb)
+mydb
+
+#PROBLEMS/QUESTIONS:
+
+#1. Can't find table in database in SQLite. 
+
+#2. How does the SQLite database work when this script is opened and run on
+#another device?
+
+#3. 
