e1330896fa6c211b795e1e27e6f7e9d38cbdb97d
jonathan653 <jonathan@fastmail.com>
2021-07-30 11:54:53 +1200

    Modify app.R in ghg-app

--- ghg-app/app.R
+++ ghg-app/app.R
@@ -8,49 +8,86 @@
 #
 
 library(shiny)
-library(plotly)
-library(DT)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-          h2("Otago University's Greenhouse Gas Emissions")
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-        downloadButton(outputId = "export_data", label = "Export Data"),
+  
+  # Application title
+  tags$br(),
+  tags$img(src = "https://www.otago.ac.nz/_assets/_gfx/logo@2x.png", width = "160px", height = "80px"),  
+  titlePanel("Greenhouse Gas Emissions"),
+  tags$h4("University of Otago's Sustainability Office"),
+  tags$br(),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      width = 4,
+      tags$h3("Student (% change)"),
+      sliderInput("bins",
+                  "Variable 1",
+                  min = 1,
+                  max = 50,
+                  value = 30),
+      sliderInput("bins",
+                  "Variable 2",
+                  min = 1,
+                  max = 50,
+                  value = 30),
+      tags$h3("Behavioural change"),
+      sliderInput("bins",
+                  "Variable 3",
+                  min = 1,
+                  max = 50,
+                  value = 30),
+      sliderInput("bins",
+                  "Variable 4",
+                  min = 1,
+                  max = 50,
+                  value = 30),
+      tags$h3("NZ electricity grid - % renewables"),
+      sliderInput("bins",
+                  "Variable 3",
+                  min = 1,
+                  max = 50,
+                  value = 30)
     ),
-        # Show a plot of the generated distribution
-        mainPanel(
-          
-           
-          
-          
-          plotOutput("distPlot")
-        )
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot"),
+      tags$br(),
+      tags$div(
+        tags$p(tags$h3("About this dashboard")), 
+        tags$p("The purpose of this dashboard is to help stakeholders easily understand the 
+                    high-level impacts of various factors (from a University context) on 
+                    net greenhouse gas emissions. 
+                    
+                    The University of Otago has set an ambitious goal of Net Carbon Zero by year 2030.
+                    Net Carbon Zero refers to the equivalent of net zero greenhouse gas emissions after 
+                    efforts to both reduce greenhouse gases and offsetting these emissions through 
+                    various strategies or initiatives.")
+      ),
+      tags$div(
+        tags$p(tags$h3("Further resources")), 
+        tags$p("Links to other relevant resources")
+      ), 
+      tags$br()
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+  
+  output$distPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  })
 }
 
 # Run the application 
