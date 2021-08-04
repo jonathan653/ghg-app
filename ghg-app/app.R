f403c3cec67e08580b78871000b197138275510c
Minquea <80385326+Minquea@users.noreply.github.com>
2021-08-04 21:27:44 +1200

    add files via upload, bretts graph in johnathans app

--- ghg-app/app.R
+++ ghg-app/app.R
@@ -11,11 +11,11 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  
   # Application title
   tags$br(),
-  tags$img(src = "https://www.otago.ac.nz/_assets/_gfx/logo@2x.png", width = "160px", height = "80px"),  
-  titlePanel("Greenhouse Gas Emissions"),
+  tags$img(src = "https://www.otago.ac.nz/_assets/_gfx/logo@2x.png", width = "160px", height = "80px"),
+  titlePanel(tags$strong("Greenhouse Gas Emissions Pathway Tool")),
+  style = "font-family: 'Open Sans', sans-serif;",
   tags$h4("University of Otago's Sustainability Office"),
   tags$br(),
   
@@ -23,31 +23,24 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       width = 4,
-      tags$h3("Student (% change)"),
+      tags$h3("Student numbers"),
+      tags$style(HTML(".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar {background: 	#00508F}")),
       sliderInput("bins",
-                  "Variable 1",
-                  min = 1,
-                  max = 50,
-                  value = 30),
-      sliderInput("bins",
-                  "Variable 2",
+                  "(Percentage change)",
                   min = 1,
                   max = 50,
                   value = 30),
       tags$h3("Behavioural change"),
+      tags$style(HTML(".js-irs-1 .irs-single, .js-irs-1 .irs-bar-edge, .js-irs-1 .irs-bar {background: 	#00508F}")),
       sliderInput("bins",
-                  "Variable 3",
+                  "(Lorem ipsum)",
                   min = 1,
                   max = 50,
                   value = 30),
+      tags$h3("NZ electricity grid"),
+      tags$style(HTML(".js-irs-2 .irs-single, .js-irs-2 .irs-bar-edge, .js-irs-2 .irs-bar {background: 	#00508F}")),
       sliderInput("bins",
-                  "Variable 4",
-                  min = 1,
-                  max = 50,
-                  value = 30),
-      tags$h3("NZ electricity grid - % renewables"),
-      sliderInput("bins",
-                  "Variable 3",
+                  "(Percentage to renewables)",
                   min = 1,
                   max = 50,
                   value = 30)
@@ -55,22 +48,35 @@ ui <- fluidPage(
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot"),
+      tags$style(" {background-color: #dd4b39;}"),
+      downloadButton('ExportPlot', 'Export as png'),
+      plotOutput("plot"),
       tags$br(),
       tags$div(
         tags$p(tags$h3("About this dashboard")), 
-        tags$p("The purpose of this dashboard is to help stakeholders easily understand the 
-                    high-level impacts of various factors (from a University context) on 
-                    net greenhouse gas emissions. 
-                    
-                    The University of Otago has set an ambitious goal of Net Carbon Zero by year 2030.
+        tags$p("The purpose of this dashboard is to help you easily understand the 
+                    high-level impacts of various factors on 
+                    net greenhouse gas emissions (from a University context)."),
+        tags$p("The University of Otago has set an ambitious goal of Net Carbon Zero by year 2030.
                     Net Carbon Zero refers to the equivalent of net zero greenhouse gas emissions after 
-                    efforts to both reduce greenhouse gases and offsetting these emissions through 
-                    various strategies or initiatives.")
+                    efforts to both reduce greenhouse gas emissions and offsetting these emissions through 
+                    various strategies or initiatives."),
+        tags$p("By engaging with this dashboard, we hope to drive awareness and behavioural change, 
+                    alongside guiding the University's strategic approach towards Net Carbon Zero.
+                    "),
+        style = "font-family: 'Open Sans', sans-serif; font-size: 16px; font-weight: normal; line-height: 1.8;"
       ),
       tags$div(
-        tags$p(tags$h3("Further resources")), 
-        tags$p("Links to other relevant resources")
+        tags$p(tags$h3("Further information")), 
+        tags$p("With the given set of variables currently i.e. student numbers, behavioural change, and NZ electricity grid, 
+        XXX hectares of trees would need to be planted by 2025 in order to reach net zero emissions in 2030. 
+        These figures are based on 1 hectare of new indigenous forest sequestering 7.8 tonnes of CO2-e by its fifth year."),
+        tags$p("To buy carbon credits from the market to offset the current emissions, the cost would be XXX - total emissions * $150."),
+        tags$p(tags$h3("Links to resources")),
+        style = "font-family: 'Open Sans', sans-serif; font-size: 16px; font-weight: normal; line-height: 1.8;",
+        tags$p(tags$a(href="https://www.otago.ac.nz/sustainability/about/", "University of Otago's Sustainability Office")),
+        tags$p(tags$a(href="https://www.otago.ac.nz/sustainability/news/otago828588.html", "University of Otago makes submission to ORC's draft 10 year plan")),
+        tags$p(tags$a(href="https://www.otago.ac.nz/sustainability/otago824241.pdf", "University of Otago's 2019 Greenhouse Gas Inventory")),
       ), 
       tags$br()
     )
@@ -80,15 +86,47 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-  output$distPlot <- renderPlot({
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  Base_Scenario <- read_excel("Project_Figures.xlsx",
+                              sheet = "4. Total base scenario", range = "A1:M11")
+  #Pivoting base scenario table to make graphing easier.
+  Base_Scenario <- Base_Scenario %>% 
+    pivot_longer(cols = `2021`:`2032`, names_to = "Year",
+                 values_to ="Carbon_Emissions",
+                 names_repair = "minimal")
+  #Rounding figures for base scenario table.
+  Base_Scenario$Carbon_Emissions <- round(Base_Scenario$Carbon_Emissions,
+                                          digits = 2)
+  #Sorting the base scenario table for easier graphing.
+  Base_Scenario <- Base_Scenario %>%
+    group_by(Year) %>% 
+    arrange(Year)
+  
+  
+  output$plot <- renderPlot({
     
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-  })
+    #Graphing the base scenario.
+    Base_Scenario_Graph <- Base_Scenario %>% 
+      ggplot() +
+      geom_col(aes(x = Year, y = Carbon_Emissions, fill = Emissions),
+               position = "stack", na.rm = TRUE) +
+      theme(legend.position="right")
+    Base_Scenario_Graph
+    
+  })  
 }
+
+# download feature
+# output$ExportPlot <- downloadHandler(
+  # file name
+  # filename <- 'plot.png',
+  # content
+  # content = function(file){
+    # create plot
+    # export(p = thePlot(), file = 'tempPlot.png')
+    # hand over the file
+    # file.copy('tempPlot.png',file)
+  # }
+# )
 
 # Run the application 
 shinyApp(ui = ui, server = server)
