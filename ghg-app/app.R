d5ac134b9d2a106c3271f51f5ea7d97666896a8b
arrmo457 <arrmo457@student.otago.ac.nz>
2021-08-11 12:29:58 +1200

    changes merged

--- ghg-app/app.R
+++ ghg-app/app.R
@@ -22,11 +22,6 @@ ui <- fluidPage(
   style = "font-family: 'Open Sans', sans-serif;",
   tags$h4("University of Otago's Sustainability Office"),
   tags$br(),
-<<<<<<< HEAD
-  
-=======
- 
->>>>>>> 2f32405ace4c24a199cf518dcf924e61bfbd17eb
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
@@ -42,24 +37,15 @@ ui <- fluidPage(
 
       tags$h3("Behavioural change"),
       tags$style(HTML(".js-irs-1 .irs-single, .js-irs-1 .irs-bar-edge, .js-irs-1 .irs-bar {background: #00508F}")),
-<<<<<<< HEAD
-      sliderInput("bins",
-=======
       sliderInput("BehaviourSlider",
 
->>>>>>> 2f32405ace4c24a199cf518dcf924e61bfbd17eb
                   "(Level of Change)",
                   min = -2,
                   max = +2,
                   value = 0),
       tags$h3("NZ electricity grid"),
-<<<<<<< HEAD
-      tags$style(HTML(".js-irs-2 .irs-single, .js-irs-2 .irs-bar-edge, .js-irs-2 .irs-bar {background: #00508F}")),
-      sliderInput("bins1", # 09aug2021 luna
-=======
       tags$style(HTML(".js-irs-2 .irs-single, .js-irs-2 .irs-bar-edge, .js-irs-2 .irs-bar {background: 	#00508F}")),
       sliderInput("ElectricitySlider",
->>>>>>> 2f32405ace4c24a199cf518dcf924e61bfbd17eb
                   "(Percentage to renewables)",
                   min = 1,
                   max = 5,
@@ -70,15 +56,9 @@ ui <- fluidPage(
     mainPanel(
       tags$style(" {background-color: #dd4b39;}"),
       downloadButton('ExportPlot', 'Export as png'),
-<<<<<<< HEAD
       
       plotOutput("plot"),
-      
-=======
-     
-      plotOutput("plot"),
-     
->>>>>>> 2f32405ace4c24a199cf518dcf924e61bfbd17eb
+
       tags$br(),
       tags$div(
         tags$p(tags$h3("About this dashboard")),
@@ -104,11 +84,7 @@ ui <- fluidPage(
         style = "font-family: 'Open Sans', sans-serif; font-size: 16px; font-weight: normal; line-height: 1.8;",
         tags$p(tags$a(href="https://www.otago.ac.nz/sustainability/about/", "University of Otago's Sustainability Office")),
         tags$p(tags$a(href="https://www.otago.ac.nz/sustainability/news/otago828588.html", "University of Otago makes submission to ORC's draft 10 year plan")),
-<<<<<<< HEAD
         tags$p(tags$a(href="https://www.otago.ac.nz/sustainability/otago824241.pdf", "University of Otago's 2019 Greenhouse Gas Inventory"))
-=======
-        tags$p(tags$a(href="https://www.otago.ac.nz/sustainability/otago824241.pdf", "University of Otago's 2019 Greenhouse Gas Inventory")),
->>>>>>> 2f32405ace4c24a199cf518dcf924e61bfbd17eb
       ),
       tags$br()
     )
@@ -125,13 +101,9 @@ server <- function(input, output) {
     pivot_longer(cols = `2021`:`2032`, names_to = "Year",
                  values_to ="Carbon_Emissions",
                  names_repair = "minimal")
-<<<<<<< HEAD
-Base_Scenario <- Base_Scenario %>% rename(Category = "Emissions")  
-=======
-  #changing type of Year
-  Base_Scenario$Year <- as.numeric(Base_Scenario$Year)
 
->>>>>>> 2f32405ace4c24a199cf518dcf924e61bfbd17eb
+Base_Scenario <- Base_Scenario %>% rename(Category = "Emissions")  
+
   #Rounding figures for base scenario table.
   Base_Scenario$Carbon_Emissions <- round(Base_Scenario$Carbon_Emissions,
                                           digits = 2)
@@ -139,18 +111,12 @@ Base_Scenario <- Base_Scenario %>% rename(Category = "Emissions")
   Base_Scenario <- Base_Scenario %>%
     group_by(Year) %>%
     arrange(Year)
-<<<<<<< HEAD
+
   
   
-  #Adjusted_Base_scenario
-  Adjusted_Multiplier <- read_excel("Project_Figures.xlsx",
-=======
- 
- 
   #Adjusted_Base_scenario
   Adjusted_Multiplier <- read_excel("Project_Figures.xlsx",
 
->>>>>>> 2f32405ace4c24a199cf518dcf924e61bfbd17eb
                                     sheet = "6.Adjustments", range = "A1:M11")
   #Pivoting Adjusted_Multiplier table to make graphing easier.
   Adjusted_Multiplier <- Adjusted_Multiplier %>%
@@ -160,7 +126,7 @@ Base_Scenario <- Base_Scenario %>% rename(Category = "Emissions")
   #Rounding figures for Adjusted_Multiplier table.
   Adjusted_Multiplier$Multipliers <- round(Adjusted_Multiplier$Multipliers,
                                        digits = 2)
-<<<<<<< HEAD
+
  Adjusted_Multiplier <- Adjusted_Multiplier %>% rename(Category = "Emissions") 
    
   
@@ -203,64 +169,7 @@ The_Complete_Table <- left_join(Base_Scenario, Adjusted_Multiplier,
          ylim(0, 50000)
        Base_Scenario_Graph
     })
-=======
 
-  #Pulls out Emissions category so we can use it in future function calls
-  BaseEmissions <- function(Category, TheYear){
-    (Base_Scenario %>% filter(Emissions == Category, Year == TheYear) %>% select(Carbon_Emissions))$Carbon_Emissions
-  }
-
-  BaseEmissions( "Staff Air Travel - domestic and international",  2022)
- 
-  #Pulls out multiplier so we can use it in future function calls
-  BaseMultiplier <- function(Category, TheYear){
-    (Adjusted_Multiplier %>% filter(Emissions == Category, Year == TheYear) %>% select(Multipliers))$Multipliers
-    #returns multiplier
-  }
-  BaseMultiplier("Staff Air Travel - domestic and international",  2022)
- 
-  #call multiplier with User input on Student Numbers
-  StudentNumbers<-function(Category, TheYear, StudentSlider){
-    BaseEmissions()* (1 + (BaseMultiplier() * (StudentSlider)))
-  }
-  
-  StudentNumbers("Staff Air Travel - domestic and international", 2022)
-  #will need to loop this function
-
-  # new_scenario <- reactive({
-  #   req(input$StudentSlider, input$...)
-  #    StudentNumbers<-reactive({
-  #req(input$StudentSlider, Emissions, BaseMultiplier)
-  #BaseEmissions()* (1 + (BaseMultiplier() * (StudentSlider)))
-#})
-  #  
-  #   vector_st_nu <- lapply(2021:2032, function(i) {
-  #     st_nu <- StudentNumbers("spreadsheet name", i, input$StudentSlider)
-  #     return(st_nu)
-  #   })
-  #  
-  #  
-  #   return(new_table)
-  # })
-  #
-  output$plot <- renderPlot({
-  #   req(new_scenario)
-    #Graphing the base scenario.
-    #geom_col(aes(x = Year, y = StudentNumbers(Category, TheYear, input$StudentSlider), fill = Emissions),
-   
-   
-
-    Base_Scenario_Graph <- Base_Scenario %>%
-      ggplot() +
-      geom_col(aes(x = Year, y = Carbon_Emissions, fill = Emissions),
-               position = position_stack(reverse = TRUE), na.rm = TRUE,
-               stat="identity", color="black") +
-      theme(legend.position="right") +
-      guides(fill = guide_legend(reverse = TRUE)) +
-      ylab("CO2 Emissions (Tonnes)")
-    Base_Scenario_Graph
-  })  
->>>>>>> 2f32405ace4c24a199cf518dcf924e61bfbd17eb
 }
 
 # download feature
@@ -277,8 +186,6 @@ The_Complete_Table <- left_join(Base_Scenario, Adjusted_Multiplier,
 # )
 
 # Run the application
-<<<<<<< HEAD
+
 shinyApp(ui = ui, server = server)
-=======
-shinyApp(ui = ui, server = server)
->>>>>>> 2f32405ace4c24a199cf518dcf924e61bfbd17eb
+
