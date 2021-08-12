1bc80c2fc3c182ff63ecdcfca6d66f84912709bc
jonathan653 <81852222+jonathan653@users.noreply.github.com>
2021-08-12 15:29:19 +1200

    Edit app.R

--- ghg-app/app.R
+++ ghg-app/app.R
@@ -11,6 +11,7 @@ library(shiny)
 library(tidyverse)
 library(readr)
 library(readxl)
+library(shinyBS)
 
 
 # Define UI for application that draws a histogram
@@ -18,73 +19,70 @@ ui <- fluidPage(
   # Application title
   tags$br(),
   tags$img(src = "https://www.otago.ac.nz/_assets/_gfx/logo@2x.png", width = "160px", height = "80px"),
-  titlePanel(tags$strong("Greenhouse Gas Emissions Pathway Tool")),
+  titlePanel(tags$strong("GHG Emissions Pathway Tool")),
   style = "font-family: 'Open Sans', sans-serif;",
-  tags$h4("University of Otago's Sustainability Office"),
   tags$br(),
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
       width = 4,
-      tags$h3("Student numbers"),
       tags$style(HTML(".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar {background: #00508F}")),
       sliderInput("StudentSlider",
-                  "(Percentage change)",
+                  tags$h3("Student numbers"),
                   min = 0,
                   max = 1,
                   value = 0,
                   step = 0.10),
+      bsPopover("StudentSlider", "Percentage change", "Growth or decline in Equivalent Full Time Students", trigger = "hover"),
 
-      tags$h3("Behavioural change"),
       tags$style(HTML(".js-irs-1 .irs-single, .js-irs-1 .irs-bar-edge, .js-irs-1 .irs-bar {background: #00508F}")),
       sliderInput("BehaviourSlider",
-
-                  "(Level of Change)",
+                  tags$h3("Behavioural change"),
                   min = -2,
                   max = +2,
                   value = 0),
-      tags$h3("NZ electricity grid"),
+      bsPopover("BehaviourSlider", "Level of change", "Conscious degree of motivation in their personal contributions towards GHG reductions", trigger = "hover",
+                options = NULL),
       tags$style(HTML(".js-irs-2 .irs-single, .js-irs-2 .irs-bar-edge, .js-irs-2 .irs-bar {background: 	#00508F}")),
       sliderInput("ElectricitySlider",
-                  "(Percentage to renewables)",
+                  tags$h3("NZ electricity grid"),
                   min = 1,
                   max = 5,
-                  value = 1)
+                  value = 1),
+      bsPopover("ElectricitySlider", "Rate of conversion", "The rate at which NZ power grid changes to renewable energy sources", trigger = "hover",
+                options = NULL)
     ),
    
     # Show a plot of the generated distribution
     mainPanel(
-      tags$style(" {background-color: #dd4b39;}"),
-      downloadButton('ExportPlot', 'Export as png'),
+      tags$div(
+        tags$p(tags$text(tags$strong("You can use this dashboard to change the high-level factors and easily understand their 
+                    impacts on the University's greenhouse gas emissions up to year 2032."))),
+        tags$style("text {color: #00508F;
+                                 font-size: 20px;
+                                 font-style: italic;
+                                 }"
+        )
+      ),
       
       plotOutput("plot"),
 
       tags$br(),
       tags$div(
-        tags$p(tags$h3("About this dashboard")),
-        tags$p("The purpose of this dashboard is to help you easily understand the
-                    high-level impacts of various factors on
-                    net greenhouse gas emissions (from a University context)."),
-        tags$p("The University of Otago has set an ambitious goal of Net Carbon Zero by year 2030.
-                    Net Carbon Zero refers to the equivalent of net zero greenhouse gas emissions after
-                    efforts to both reduce greenhouse gas emissions and offsetting these emissions through
-                    various strategies or initiatives."),
-        tags$p("By engaging with this dashboard, we hope to drive awareness and behavioural change,
-                    alongside guiding the University's strategic approach towards Net Carbon Zero.
-                    "),
-        style = "font-family: 'Open Sans', sans-serif; font-size: 16px; font-weight: normal; line-height: 1.8;"
-      ),
-      tags$div(
-        tags$p(tags$h3("Further information")),
         tags$p("With the given set of variables currently i.e. student numbers, behavioural change, and NZ electricity grid,
         XXX hectares of trees would need to be planted by 2025 in order to reach net zero emissions in 2030.
         These figures are based on 1 hectare of new indigenous forest sequestering 7.8 tonnes of CO2-e by its fifth year."),
         tags$p("To buy carbon credits from the market to offset the current emissions, the cost would be XXX - total emissions * $150."),
-        tags$p(tags$h3("Links to resources")),
+        tags$p(tags$h3("Further information")),
         style = "font-family: 'Open Sans', sans-serif; font-size: 16px; font-weight: normal; line-height: 1.8;",
         tags$p(tags$a(href="https://www.otago.ac.nz/sustainability/about/", "University of Otago's Sustainability Office")),
         tags$p(tags$a(href="https://www.otago.ac.nz/sustainability/news/otago828588.html", "University of Otago makes submission to ORC's draft 10 year plan")),
         tags$p(tags$a(href="https://www.otago.ac.nz/sustainability/otago824241.pdf", "University of Otago's 2019 Greenhouse Gas Inventory"))
+      ),
+      tags$style("p {color: blue;
+                                 font-size: 16px;
+                                 font-style: normal;
+                                 }"
       ),
       tags$br()
     )
@@ -240,19 +238,6 @@ The_Complete_Table <- left_join(The_Complete_Table, Electricity_Multipliers,
     })
 
 }
-
-# download feature
-# output$ExportPlot <- downloadHandler(
-# file name
-# filename <- 'plot.png',
-# content
-# content = function(file){
-# create plot
-# export(p = thePlot(), file = 'tempPlot.png')
-# hand over the file
-# file.copy('tempPlot.png',file)
-# }
-# )
 
 # Run the application
 
