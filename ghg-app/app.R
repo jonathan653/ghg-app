bcc1198d4ede02164235d26087c3967ad7e903a6
arrmo457 <arrmo457@student.otago.ac.nz>
2021-08-23 20:09:15 +1200

    prep for merge

--- ghg-app/app.R
+++ ghg-app/app.R
@@ -9,7 +9,6 @@
 
 library(shiny)
 library(tidyverse)
-library(readr)
 library(readxl)
 library(shinyBS)
 
@@ -32,7 +31,7 @@ ui <- fluidPage(
                   min = -.50,
                   max = 1,
                   value = 0,
-                  step = 0.25),
+                  step = 0.10),
       bsPopover("StudentSlider" , "Percentage of change in student numbers" , "The change in Equivalent Full Time Students can affect the amount of resources the University requires, and therefore indirectly impacts on greenhouse gas emissions. Note: the slider bar moves on a 10% incremental increase in student numbers i.e. 0.1 = 10%", trigger = "hover"),
       
       tags$style(HTML(".js-irs-1 .irs-single, .js-irs-1 .irs-bar-edge, .js-irs-1 .irs-bar {background: #00508F}")),
